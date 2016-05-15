e87c0500e4bc34e25145b0d29a394dfe6c50e96c
Charles Zhang <charleszhang@welove-inc.com>
2016-05-15 22:00:17 +0800

    创建初始工程

---
+++ AndroidDemos/app/src/androidTest/java/com/zhangtielei/demos/async/programming/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.zhangtielei.demos.async.programming;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}

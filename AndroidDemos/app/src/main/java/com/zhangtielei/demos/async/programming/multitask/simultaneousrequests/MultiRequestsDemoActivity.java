1951fb19a4b34a922368586b5bcd40aabf484e8a
Charles Zhang <charleszhang@welove-inc.com>
2016-05-17 15:07:21 +0800

    两个请求同时发生的例子完

--- AndroidDemos/app/src/main/java/com/zhangtielei/demos/async/programming/multitask/simultaneousrequests/MultiRequestsDemoActivity.java
+++ AndroidDemos/app/src/main/java/com/zhangtielei/demos/async/programming/multitask/simultaneousrequests/MultiRequestsDemoActivity.java
@@ -15,21 +15,120 @@
  */
 package com.zhangtielei.demos.async.programming.multitask.simultaneousrequests;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import com.zhangtielei.demos.async.programming.R;
+import com.zhangtielei.demos.async.programming.multitask.http.HttpListener;
+import com.zhangtielei.demos.async.programming.multitask.http.HttpResult;
+import com.zhangtielei.demos.async.programming.multitask.http.HttpService;
+import com.zhangtielei.demos.async.programming.multitask.http.mock.MockHttpService;
+import com.zhangtielei.demos.async.programming.multitask.simultaneousrequests.model.HttpRequest1;
+import com.zhangtielei.demos.async.programming.multitask.simultaneousrequests.model.HttpRequest2;
+import com.zhangtielei.demos.async.programming.multitask.simultaneousrequests.model.HttpResponse1;
+import com.zhangtielei.demos.async.programming.multitask.simultaneousrequests.model.HttpResponse2;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 展示两个请求同时发生的异步处理过程.
  */
 public class MultiRequestsDemoActivity extends AppCompatActivity {
+    private HttpService httpService = new MockHttpService();
+    /**
+     * 缓存各个请求结果的Map
+     */
+    private Map<String, Object> httpResults = new HashMap<String, Object>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multi_requests_demo);
+
+        //同时发起两个异步请求
+        httpService.doRequest("http://...", new HttpRequest1(),
+                new HttpListener<HttpRequest1, HttpResponse1>() {
+                    @Override
+                    public void doResult(String apiUrl,
+                                         HttpRequest1 request,
+                                         HttpResult<HttpResponse1> result,
+                                         Object contextData) {
+                        //将请求结果缓存下来
+                        httpResults.put("request-1", result);
+                        if (checkAllHttpResultsReady()) {
+                            HttpResult<HttpResponse1> result1 = result;
+                            HttpResult<HttpResponse2> result2 = (HttpResult<HttpResponse2>) httpResults.get("request-2");
+                            if (checkAllHttpResultsSuccess()) {
+                                processHttpResponses(result1.getResponse(), result2.getResponse());
+                            }
+                            else {
+                                processHttpErrorResults(result1, result2);
+                            }
+                        }
+                    }
+                },
+                null);
+        httpService.doRequest("http://...", new HttpRequest2(),
+                new HttpListener<HttpRequest2, HttpResponse2>() {
+                    @Override
+                    public void doResult(String apiUrl,
+                                         HttpRequest2 request,
+                                         HttpResult<HttpResponse2> result,
+                                         Object contextData) {
+                        //将请求结果缓存下来
+                        httpResults.put("request-2", result);
+                        if (checkAllHttpResultsReady()) {
+                            HttpResult<HttpResponse1> result1 = (HttpResult<HttpResponse1>) httpResults.get("request-1");
+                            HttpResult<HttpResponse2> result2 = result;
+                            if (checkAllHttpResultsSuccess()) {
+                                processHttpResponses(result1.getResponse(), result2.getResponse());
+                            }
+                            else {
+                                processHttpErrorResults(result1, result2);
+                            }
+                        }
+                    }
+                },
+                null);
+    }
+
+    /**
+     * 检查是否所有请求都有结果了
+     * @return
+     */
+    private boolean checkAllHttpResultsReady() {
+        int requestsCount = 2;
+        for (int i = 1; i <= requestsCount; i++) {
+            if (httpResults.get("request-" + i) == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 检查是否所有请求都成功了
+     * @return
+     */
+    private boolean checkAllHttpResultsSuccess() {
+        int requestsCount = 2;
+        for (int i = 1; i <= requestsCount; i++) {
+            HttpResult<?> result = (HttpResult<?>) httpResults.get("request-" + i);
+            if (result == null || result.getErrorCode() != HttpResult.SUCCESS) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void processHttpResponses(HttpResponse1 response1, HttpResponse2 response2) {
+        //TODO: 更新UI, 展示请求结果. 省略此处代码
+    }
+
+    private void processHttpErrorResults(HttpResult<HttpResponse1> result1, HttpResult<HttpResponse2> result2) {
+        //TODO: 更新UI,展示错误. 省略此处代码
     }
 
     @Override
